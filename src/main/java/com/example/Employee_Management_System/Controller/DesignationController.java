671f4f9bee1635c88cd9d0f129128dc8e7033273
jayshah99 <69785767+jayshah99@users.noreply.github.com>
2020-09-07 13:14:21 +0530

    Add files via upload

---
+++ src/main/java/com/example/Employee_Management_System/Controller/DesignationController.java
@@ -0,0 +1,48 @@
+package com.example.Employee_Management_System.Controller;
+
+import com.example.Employee_Management_System.Resources.model.Designation;
+import com.example.Employee_Management_System.Resources.model.Employee;
+import com.example.Employee_Management_System.Resources.request.EmployeeRequest;
+import com.example.Employee_Management_System.Service.DesignationService;
+import com.example.Employee_Management_System.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+public class DesignationController {
+
+    @Autowired
+    DesignationService designationService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/designations")
+    public Designation addDesignation (@RequestBody Designation designation){
+        return designationService.addDesignation(designation);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/designations")
+    public List<Designation> designationList() {
+        return designationService.designationsList();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("designations/{id}")
+    public void delete(@PathVariable(value = "id") int id) {
+        designationService.delete(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/designations")
+    public Designation update(@RequestBody Designation designation) {
+        return designationService.update(designation);
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/designations/{id}")
+    public Designation get(@PathVariable("id") int id) {
+        return designationService.getById(id);
+    }
+}
---
+++ src/main/java/com/example/Employee_Management_System/Resources/model/Employee.java
@@ -0,0 +1,54 @@
+package com.example.Employee_Management_System.Resources.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Employee {
+    @Id
+    @Column
+    @GeneratedValue
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private int age;
+    @Column
+    private String address;
+    @Column
+    private String gender;
+    @Column
+    private String email;
+    @Column
+    private String phone_number;
+
+    @OneToMany(targetEntity = Project.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "empid", referencedColumnName = "id")
+    private List<Project> project;
+
+    @OneToMany(targetEntity = Designation.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "empid", referencedColumnName = "id")
+    private List<Designation> designation;
+
+    public Employee(String name, String address,int age, String gender, String email, String phone_number, List<Project> project, List<Designation> designation) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.gender = gender;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.project = project;
+        this.designation = designation;
+    }
+
+
+}
