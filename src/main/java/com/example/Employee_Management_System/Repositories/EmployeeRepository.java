a0bfd2e56f2597b1cdfb5f65c8a658fe46200c47
jay shah <jaypshah99@gmail.com>
2020-09-29 16:04:40 +0530

    Added a new query method which is used in updateById function.

--- src/main/java/com/example/Employee_Management_System/Repositories/EmployeeRepository.java
+++ src/main/java/com/example/Employee_Management_System/Repositories/EmployeeRepository.java
@@ -9,12 +9,14 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-//    @Query("select e from Employee e where e.name = ?1")
         @Query("select e from Employee e where e.name like %:keyword%")
-      List<Employee> findByName( @Param("keyword") String name);
+        List<Employee> findByName(@Param("keyword") String name);
 
-        //To check whether the email entered is already present or not
+    //To check whether the email entered is already present or not
         boolean existsEmployeeByEmail(String email);
 
         boolean existsEmployeeByPhoneNumber(String phoneNumber);
+
+        @Query(value = "select * from employee where (email=?1 or phone_number=?2) and id <>?3", nativeQuery = true)
+        List<Employee> updatedEmailOrPhoneNumberExists( String email, String phone, int id);
 }
